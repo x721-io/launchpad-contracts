94982db9f904b8dc7a172034bb279ba7d79a77a0
MacOs <dangkimbao1999@gmail.com>
2024-12-26 16:45:54 +0700

    [UPDATE] pump openzeppelin

--- scripts/deploy/deploy-ProjectManager.js
+++ scripts/deploy/deploy-ProjectManager.js
@@ -1,9 +1,10 @@
 async function main() {
   const [deployer] = await ethers.getSigners();
+  console.log(deployer)
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const u2uProjectManager = await ethers.deployContract("U2UProjectManager");
+  const u2uProjectManager = await ethers.deployContract("U2UProjectManager", ["0x2743eEC46576f76f47334569074242F3D9a90B44"]);
 
   console.log("u2uProjectManager address:", await u2uProjectManager.getAddress());
 }
